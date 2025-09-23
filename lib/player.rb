17a55c9ec45a81d96a572eff3bc3c7ede8979c08
Todiun <joquch@hotmail.co.uk>
2025-09-23 19:16:19 +0200

    2nd iteration

--- lib/player.rb
+++ lib/player.rb
@@ -10,4 +10,24 @@ class Player
   def status
     puts "#{@name} has #{@lp} life points"
   end
+  
+  def gets_damage(damage)
+  @lp -= damage
+    if @lp <= 0
+    @lp = 0
+    puts "#{@name} has been slain !"
+    end
+  end
+  
+  def attacks(other_player)
+    puts "#{@name} attacks #{other_player.name}"
+    damage = compute_damage
+    other_player.gets_damage(damage)
+    puts "Target player lost #{damage} life points"
+  end
+
+  def compute_damage
+    rand(1..6)
+  end
+
 end
